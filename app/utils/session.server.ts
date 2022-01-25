65401a5a67a0752c7bf75f5916996b4765048293
Omar <mihilmy@yahoo.com>
2022-01-24 17:07:36 -0800

    Adding logout capability

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -56,6 +56,27 @@ export async function requireUserId(request: Request, redirectTo: string = new U
   return userId;
 }
 
+export async function getUser(request: Request) {
+  const userId = await getUserId(request);
+  if (typeof userId !== "string") {
+    return null;
+  }
+
+  try {
+    const user = await db.user.findUnique({ where: { id: userId } });
+    return user;
+  } catch {
+    throw logout(request);
+  }
+}
+
+export async function logout(request: Request) {
+  const session = await storage.getSession(request.headers.get("Cookie"));
+  return redirect("/login", {
+    headers: { "Set-Cookie": await storage.destroySession(session) }
+  });
+}
+
 type LoginForm = {
   username: string;
   password: string;
