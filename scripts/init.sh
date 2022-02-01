717b30cfc97630eeac77d4424101c585436ac947
mihilmy <mihilmy@yahoo.com>
2022-02-01 05:48:49 +0000

    Adding init script for deploying

---
+++ scripts/init.sh
@@ -0,0 +1,7 @@
+# Initialize
+copilot init --app remix-apps \
+             --name jokes-app \
+             --type 'Request-Driven Web Service' \
+             --dockerfile './Dockerfile' \
+             --port 80 \
+             --deploy
